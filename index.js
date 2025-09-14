879d1e54194526653281ff8189b726f5a8ecc657
Viitorsoares <viitorgab.soares@gmail.com>
2025-09-14 17:20:01 -0300

    Finalizando camada de controles

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import Course from "./Models/course.js";
 
 let course = new Course(
     "134679",
-    "Curso de responsividade css",
-    "25-06-2027",
-    "8 horas",
-    "180"
+    "Curso de Javascript",
+    "10-09-2025",
+    "10 horas",
+    "1000"
 );
 
-course.toRecord().then(() => {
-    console.log(`Curso gravado com sucesso`)
-}).catch((error) => {
-    console.log(`Erro ao gravar o pacote ${error}`)
-})
+// course.toRecord().then(() => {
+//     console.log(`Curso gravado com sucesso`)
+// }).catch((error) => {
+//     console.log(`Erro ao gravar o curso ${error}`)
+// })
 
 
 // course.consult().then((listOfCourses) => {
