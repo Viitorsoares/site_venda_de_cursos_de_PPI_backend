12b5a1fe70c48d2be3ec87387b8eb7ad4c23df52
Viitorsoares <viitorgab.soares@gmail.com>
2025-09-14 21:26:18 -0300

    Finalizando

--- index.js
+++ index.js
@@ -14,37 +14,3 @@ app.listen(door, host, () => {
     console.log(`Servidor rodando em http://${host}:${door}`);
 });
 
-// let course = new Course(
-//     "134679",
-//     "Curso de Javascript",
-//     "10-09-2025",
-//     "10 horas",
-//     "1000"
-// );
-
-// course.toRecord().then(() => {
-//     console.log(`Curso gravado com sucesso`)
-// }).catch((error) => {
-//     console.log(`Erro ao gravar o curso ${error}`)
-// })
-
-
-// course.consult().then((listOfCourses) => {
-//     for (const course of listOfCourses) {
-//         console.log(course.toJSON())
-//     }
-// })
-
-
-// course.delete().then(() => {
-//     console.log("Curso deletado com sucesso!");
-// }).catch((error) => {
-//     console.log("Erro ao deletar o curso: " + error);
-// })
-
-
-// course.toAlter().then(() => {
-//     console.log("Curso alterado com sucesso!");
-// }).catch((error) => {
-//     console.log("Erro ao alterar o curso: " + error);
-// });
